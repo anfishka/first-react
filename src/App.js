04d7c6b3a02d6e8cda745b59146816ff34bbf7f1
Hanna <venera131916@gmail.com>
2022-07-10 15:07:24 +0300

    feat: add component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import pokemon from './pokemon.json'
+
+console.log(pokemon)
+//structure pokemon = [{-},]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Pokemon Table</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+        <th>Type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/*switch to JS*/}
+        {pokemon.map((onepokemon)=>(
+          //this is HTML
+          <tr>
+            <td>
+                {/*swith to JS */}
+                {onepokemon.name.english}
+            </td>
+            <td>
+                {onepokemon.type.join(", ")}
+            </td>
+        </tr>
+      ))
+      }
+      </tbody>
+    </table>
     </div>
   );
 }
